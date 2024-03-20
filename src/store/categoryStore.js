50318b0aa0a97729cc9c298a68fdfb19a46d0a41
hyewon93 <baehw93@gmail.com>
2024-03-20 16:44:44 -0700

    Add 'persist' for categoryStore to prevent refresh

--- src/store/categoryStore.js
+++ src/store/categoryStore.js
@@ -1,8 +1,17 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
-const useCategoryStore = create((set) => ({
-    categories: [],
-    setCategory: (categories) => set({categories})
-}));
+const useCategoryStore = create(
+    persist(
+        (set) => ({
+            categories: [],
+            setCategory: (categories) => set({categories})
+        }),
+        {
+            name: 'user-category',
+            storage: createJSONStorage(() => sessionStorage),
+        }
+    )
+);
 
 export default useCategoryStore;
